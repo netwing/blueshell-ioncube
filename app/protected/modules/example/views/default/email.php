52cce94fb6a494995e1c0e366bf95528badd4c24
Emanuele Deserti <emanuele.deserti@netwing.it>
2014-01-20 17:15:44 +0100

    Release v2.0.0

--- app/protected/modules/example/views/default/email.php
+++
@@ -1,9 +0,0 @@
-<?php //0046a
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
-?>
-HR+cPvmeEqR53O1fX96kPGQL+y3Qrtyzitb2gh+iHYrjVNXPd3OtzcEiG4lhZ0tPHamq6VtNAZVG
-rT6w5DvZQb74Gzfkqk33BRvIVWHzggk4bnXB5k10DMLo0A1X28RIPGdnX0IlhCb/s0XaCzLniJYF
-U+XWiJh/d1n31WJGL9AXkkxpXTNmNi4fyodPomsod6HCHWtfAyLfPv39YMU5JPlIw+pSxLHsCBxi
-81JbYfAtPNVL1J9KZzJYd0MdN8nUzM+wPoTgXVSD3frZpJ9dboskIJy3bco2Kh9lLeUqhAVbWRSk
-M4/Pb5PTXj4LLUQi2jG8kpVyPiRwrMMdxLsoCN3KStBcubzabTLgmPd2ligMi3uuchksWtT6nQAB
-JCmHYupqJJrc67yRPwdIASIuZkB1iNCmQd4=
