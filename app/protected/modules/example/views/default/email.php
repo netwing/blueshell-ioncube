90b2dabe34580b74ea353cec50a40671e413ce8d
Emanuele Deserti <emanuele.deserti@netwing.it>
2014-01-20 17:22:19 +0100

    Release v2.0.0

--- app/protected/modules/example/views/default/email.php
+++
@@ -1,9 +0,0 @@
-<?php //0046a
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
-?>
-HR+cPmPHgeDokYwq7+W/e8MkUTiqn3D1+Uo84SHTk2zvdwKCg/yvxCsNlpQjSF1szxhBZtovkO+j
-1ExbNIBH57/a3oEeVXZgCkeD6/gSoSf2sbpY3bkGMeaZSsykAnba1iwZELSrGxC+48ZCqJGv4342
-LZvTCZHkm5NfdKu5uaQ1ERIXDGstzOyB2dNA2IcMf8+jzkyVtykbn5/TogWMjZb9BuZC4bad+11X
-ix3zLA3/1w//A1RZ9DPWigX+5PsTUCFvvH6+Pxvtd/OjOsMJQCM6nfdqKHpiyxstMbP+kLhRn1xr
-4R0vsMDzDA9Az+8jIS3etga8Dosadp3pgGBjAlI9o1SD5D3Hhkx50rtdJ/S9kJH67gK3FUGbxO9e
-kEnbFTf3v6RzV5pnDe7g3+2GIQ47mQz8BNlM
--- templates/loginfooter.tpl
+++
@@ -1,6 +0,0 @@
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="jquery/jquery.min.js"></script>
-	<!-- Include all compiled plugins (below), or include individual files as needed -->
-	<script src="bootstrap/js/bootstrap.min.js"></script>
-</body>
-</html>
